acb102c86858a15a18f25b24777a85ebe55ec48e
Mike Downes <mikedownes.dev@gmail.com>
2022-09-20 13:08:05 -0400

    Chapter 9

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-
+      <Link prefetch={true} href={{
+          pathname: '/[username]',
+          query: { username: 'mikedownesdev'}
+        }}>
+        <a>Mike's Profile</a>
+      </Link>
     </div>
   )
 }
