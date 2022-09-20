42f0c062422d5dae1b3e5c16025a8defbb441f63
Mike Downes <mikedownes.dev@gmail.com>
2022-09-20 13:11:47 -0400

    Chapter 10

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+
+import Loader from '../components/Loader';
 
 export default function Home() {
   return (
     <div>
-      <Link prefetch={true} href={{
-          pathname: '/[username]',
-          query: { username: 'mikedownesdev'}
-        }}>
-        <a>Mike's Profile</a>
-      </Link>
+      <Loader show />
     </div>
+    // <div>
+    //   <Link prefetch={true} href={{
+    //       pathname: '/[username]',
+    //       query: { username: 'mikedownesdev'}
+    //     }}>
+    //     <a>Mike's Profile</a>
+    //   </Link>
+    // </div>
   )
 }
